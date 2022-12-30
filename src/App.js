d6364840dd2174346149a58c04813212cf0b7ba9
abhinavanand-fed <abhinavanand080@gmail.com>
2022-12-30 19:45:59 +0530

    Changes

--- src/App.js
+++ src/App.js
@@ -1,24 +1,84 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import {
+  BrowserRouter as Router,
+  Route,
+  Link,
+  useHistory,
+  useLocation
+} from 'react-router-dom';
+
+function Page1() {
+  const [formData, setFormData] = useState({});
+  const history = useHistory();
+
+  function handleChange(event) {
+    const { name, value } = event.target;
+    setFormData(prevData => ({ ...prevData, [name]: value }));
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    history.push({
+      pathname: '/page2',
+      state: { formData }
+    });
+  }
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <label>
+        Name:
+        <input type="text" name="name" onChange={handleChange} />
+      </label>
+      <br />
+      <label>
+        Email:
+        <input type="email" name="email" onChange={handleChange} />
+      </label>
+      <br />
+      <button type="submit">Submit</button>
+    </form>
+  );
+}
+
+function Page2() {
+  const location = useLocation();
+  const formData = location.state.formData;
+
+  return (
+    <table>
+      <tbody>
+        <tr>
+          <th>Name</th>
+          <td>{formData.name}</td>
+        </tr>
+        <tr>
+          <th>Email</th>
+          <td>{formData.email}</td>
+        </tr>
+      </tbody>
+    </table>
+  );
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Page 1</Link>
+            </li>
+            <li>
+              <Link to="/page2">Page 2</Link>
+            </li>
+          </ul>
+        </nav>
+        <Route path="/" exact component={Page1} />
+        <Route path="/page2" component={Page2} />
+      </div>
+    </Router>
   );
 }
 
